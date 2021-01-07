b1004b858c0e213f64fcdc746778f97821d79179
Joseph Ikpontu <Jikpontu@gmail.com>
2021-01-07 14:39:44 +0100

    update to regtype, specialization and providers dropdown

---
+++ medicloud.emr.api/Entities/ApplicationUserLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace medicloud.emr.api.Entities
+{
+    public class ApplicationUserLocation
+    {
+        public int appuserlocationid { get; set; }
+        public int appuserid { get; set; }
+        public int specid { get; set; }
+        public int locationid { get; set; }
+    }
+}
