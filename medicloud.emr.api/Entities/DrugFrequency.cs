81ff5c5e328c4d8be78ae7ba5c1110f384895545
Aghoghomena <aghoghojao@gmail.com>
2021-01-18 18:39:15 +0100

    Prescriotion API created

---
+++ medicloud.emr.api/Entities/DrugFrequency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace medicloud.emr.api.Entities
+{
+    public partial class DrugFrequency
+    {
+        public int FrequencyId { get; set; }
+        public string Description { get; set; }
+        public string Abbreviation { get; set; }
+        public decimal? Frequency { get; set; }
+        public int? Sequence { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? CommonFrequency { get; set; }
+        public bool? IsTimingRequired { get; set; }
+    }
+}
---
+++ medicloud.emr.api/Entities/DrugRoute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace medicloud.emr.api.Entities
+{
+    public partial class DrugRoute
+    {
+        public int RouteId { get; set; }
+        public string RouteName { get; set; }
+        public string Description { get; set; }
+        public int? SequenceNo { get; set; }
+        public bool? IsDefault { get; set; }
+        public bool? Isactive { get; set; }
+        public int? EncodedBy { get; set; }
+        public DateTime? Encodeddate { get; set; }
+    }
+}
