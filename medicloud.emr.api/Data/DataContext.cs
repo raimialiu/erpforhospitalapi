065ce09b71be124c3ee156e105cafed9f0377cfd
olatundedatascience <44260733+olatundedatascience@users.noreply.github.com>
2021-01-27 09:47:35 +0100

    removed brandid from consultation favorites

--- medicloud.emr.api/Data/DataContext.cs
+++ medicloud.emr.api/Data/DataContext.cs
@@ -303,7 +303,7 @@ namespace medicloud.emr.api.Data
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
 
                // optionsBuilder.UseSqlServer("Data Source= hnltestuatlhis.database.windows.net;Initial Catalog=medismartsemr_db_test;Persist Security Info=True;User ID=!lagadmin!;Password=8mT@92EFQi0x;MultipleActiveResultSets=True");
-             // optionsBuilder.UseSqlServer("Data Source=FCMB-IT-L16582\\TUNDE;Initial Catalog=medismartsemr_db;Persist Security Info=True;User ID=olatunde;Password=DVorak@23000;MultipleActiveResultSets=True");
+             
 
               // optionsBuilder.UseSqlServer("Data Source=52.251.49.79;Initial Catalog=medismartsemr_db;Persist Security Info=True;User ID=medismarts;Password=md2015@tech");
              optionsBuilder.UseSqlServer("Data Source=hnltestuatlhis.database.windows.net;Initial Catalog=medismartsemr_db_dev;Persist Security Info=True;User ID=!lagadmin!;Password=8mT@92EFQi0x;MultipleActiveResultSets=True",
