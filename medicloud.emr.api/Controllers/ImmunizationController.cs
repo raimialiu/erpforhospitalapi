7462ec3f61ebfa0b79808f4ff77eedabee3bcea4
Aliu Raimi <olatundedatascience@outlook.com>
2021-01-31 17:48:21 +0100

    revert all immunization schedule to initial schedule

--- medicloud.emr.api/Controllers/ImmunizationController.cs
+++ medicloud.emr.api/Controllers/ImmunizationController.cs
@@ -32,8 +32,8 @@ namespace medicloud.emr.api.Controllers
         [HttpGet]
         public async Task<IActionResult> AllImunizationSchedule()
         {
-           //var all =await _conn.QueryAsync("select * from Immunization_Schedule a join EmrImmunizationMaster b on a.immunizationid = b.Immunizationid");
-          var all = await _conn.QueryAsync("select a.*, b.immunizationname, b.isactive,b.cptcode, b.encodeddate, c.duedate, c.givendatetime, c.remarks, c.batchno, c.brandid, c.givenby from Immunization_Schedule a join EmrImmunizationMaster b on a.immunizationid = b.Immunizationid join immunization_details c on a.scheduleid = c.scheduleid");
+           var all =await _conn.QueryAsync("select * from Immunization_Schedule a join EmrImmunizationMaster b on a.immunizationid = b.Immunizationid");
+         // var all = await _conn.QueryAsync("select a.*, b.immunizationname, b.isactive,b.cptcode, b.encodeddate, c.duedate, c.givendatetime, c.remarks, c.batchno, c.brandid, c.givenby from Immunization_Schedule a join EmrImmunizationMaster b on a.immunizationid = b.Immunizationid join immunization_details c on a.scheduleid = c.scheduleid");
 
             return Ok(all);
         }
