5df7533c1ee16c4b465d5613d954cb2c972befc3
Joseph Ikpontu <Jikpontu@gmail.com>
2021-02-02 12:34:20 +0100

    update to billing and add billing invoice

--- medicloud.emr.api/DTOs/PatientPayorTypesDto.cs
+++ medicloud.emr.api/DTOs/PatientPayorTypesDto.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace medicloud.emr.api.Entities
+namespace medicloud.emr.api.DTOs
 {
-    public partial class PatientPayorTypes
+    public class PatientPayorTypesDto
     {
         public int Id { get; set; }
         public string Patientid { get; set; }
         public string Payor { get; set; }
+        public int? Payerid { get; set; }
         public string accountcategory { get; set; }
         public string plantype { get; set; }
         public string sponsor { get; set; }
+        public string PayerType { get; set; }
         public int? accountid { get; set; }
     }
 }
